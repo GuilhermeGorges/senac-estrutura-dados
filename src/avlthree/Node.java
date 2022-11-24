e8ab56d0074606c9d03b02ee6dafc11dca2f1d69
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-11-23 21:34:57 -0300

    NODE

--- src/avlthree/Node.java
+++ src/avlthree/Node.java
@@ -5,5 +5,63 @@ public class Node {
     private Object element;
     private Node left;
     private Node right;
+    private Node parent;
+    private long b;
 
+    public Node(long id, Object element, Node left, Node right, Node parent, long b) {
+        this.id = id;
+        this.element = element;
+        this.left = left;
+        this.right = right;
+        this.parent = parent;
+        this.b = b;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public long getB() {
+        return b;
+    }
+
+    public void setB(long b) {
+        this.b = b;
+    }
 }
